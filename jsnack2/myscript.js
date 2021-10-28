f8d3af506b99ec9f1114a39510b60ab6b625057c
JackSparring420 <emanuele.arconte94@gmail.com>
2021-10-28 17:09:56 +0200

    exercise 1 and 2 completed

---
+++ jsnack2/myscript.js
@@ -0,0 +1,27 @@
+// L’utente inserisce due parole in successione, con due prompt.
+const wordFirst = prompt("inserisci la prima parola");
+const wordSecond = prompt("inserisci ls deconda parola");
+// controllo i valori in console
+console.log(wordFirst.length);
+console.log(wordSecond.length);
+
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+let stampa = document.getElementById("wordMax");
+// Stampo il maggiore.
+if (wordFirst.length > wordSecond.length) {
+    console.log (wordFirst);
+    stampa.innerHTML = wordFirst;
+} else if (wordFirst.length === wordSecond.length) {
+    console.log (wordFirst + wordSecond);
+    stampa.innerHTML = wordFirst + wordSecond;
+} else if (wordFirst.length < wordSecond.length) {
+    console.log (wordSecond);
+    stampa.innerHTML = wordSecond;
+};
+
+
+
+
+
+
+
